82e6ec5c01b8e9e1eb91a39bf3ed81ece6bcecf9
aaron276h <aaron@determined.ai>
2020-03-12 15:14:36 -0400

    Add Licensing.

--- yogadl/_lmdb_handler.py
+++ yogadl/_lmdb_handler.py
@@ -1,3 +1,17 @@
+# Copyright 2020 Determined AI. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 import logging
 import pathlib
 import pickle
@@ -7,6 +21,22 @@ from typing import Any, cast, Generator, List
 import lmdb
 
 
+# serialize_generator_to_lmdb is derived from:
+#
+# Copyright 2016 Yuxin Wu. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 def serialize_generator_to_lmdb(
     dataset_generator: Generator,
     data_shapes: Any,
