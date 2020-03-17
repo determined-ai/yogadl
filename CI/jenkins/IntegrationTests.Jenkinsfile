79315082e8833f73465ec73f5bf049b9bd18ffe6
aaron276h <aaron@determined.ai>
2020-03-17 10:18:56 -0400

    Update CI tests to support TF2.
    DET-2611  #Done.

--- CI/jenkins/IntegrationTests.Jenkinsfile
+++ CI/jenkins/IntegrationTests.Jenkinsfile
@@ -1,13 +1,23 @@
 pipeline {
   agent any
   stages {
-    stage('IntegrationTests') {
+    stage('TF2Tests') {
       agent { label 'general' }
       steps {
         sh 'virtualenv --python="$(command -v python3.6)" --no-site-packages venv'
         sh "venv/bin/python -m pip install -r requirements.txt"
+        sh "venv/bin/python -m pip install tensorflow==2.1.0"
         sh "venv/bin/python -m pytest tests/integration/local/"
         sh "venv/bin/python -m pytest tests/integration/aws/"
+      }
+    }
+    stage('TF1Tests') {
+      agent { label 'general' }
+      steps {
+        sh 'virtualenv --python="$(command -v python3.6)" --no-site-packages venv'
+        sh "venv/bin/python -m pip install -r requirements.txt"
+        sh "venv/bin/python -m pip install tensorflow==1.14.0"
+        sh "venv/bin/python -m pytest tests/integration/local/"
       }
     }
   }
--- tests/integration/local/test_lfs_system.py
+++ tests/integration/local/test_lfs_system.py
@@ -1,6 +1,3 @@
-import numpy as np
-import tensorflow as tf
-
 import tests.integration.util as util  # noqa: I202, I100
 
 from yogadl import dataref, storage, tensorflow
@@ -19,27 +16,13 @@ def test_mnist_single_threaded() -> None:
 
     @lfs_storage.cacheable(dataset_id=dataset_id, dataset_version=dataset_version)
     def make_dataset() -> dataref.LMDBDataRef:
-        return util.make_mnist_train_dataset()  # type: ignore
+        return util.make_mnist_test_dataset()  # type: ignore
 
     stream_from_cache = make_dataset().stream()
     dataset_from_stream = tensorflow.make_tf_dataset(stream_from_cache)
-    original_dataset = util.make_mnist_train_dataset()
+    original_dataset = util.make_mnist_test_dataset()
 
-    next_element_from_stream = dataset_from_stream.make_one_shot_iterator().get_next()
-    next_element_from_orig = original_dataset.make_one_shot_iterator().get_next()
-
-    with tf.Session() as sess:
-        data_samples = 0
-        while True:
-            try:
-                element_from_stream = sess.run(next_element_from_stream)
-                element_from_dataset = sess.run(next_element_from_orig)
-                assert element_from_stream["label"] == element_from_dataset["label"]
-                assert np.array_equal(element_from_stream["image"], element_from_dataset["image"])
-                data_samples += 1
-            except tf.errors.OutOfRangeError:
-                break
-
+    data_samples = util.compare_datasets(original_dataset, dataset_from_stream)
     assert data_samples == 10000
     assert stream_from_cache.length == data_samples
     util.cleanup_lfs_storage(
