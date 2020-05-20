7edd5d12e00068877027ad103ee8fd36719b5d65
aaron276h <aaron@determined.ai>
2020-05-20 10:32:37 -0400

    Bump version to 0.1.1.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="yogadl",
-    version="0.1.0",
+    version="0.1.1",
     author="Determined AI",
     author_email="hello@determined.ai",
     url="https://www.github.com/determined-ai/yogadl/",
